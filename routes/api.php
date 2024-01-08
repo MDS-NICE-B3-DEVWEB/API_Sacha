5bbba91a1220d282ae55d9aaed71e7a6da17fef7
Sacha Fougeras <fougerassacha@gmail.com>
2024-01-08 14:24:32 +0100

    droits nouvelles fonctionnalités

--- routes/api.php
+++ routes/api.php
@@ -18,24 +18,31 @@ use App\Http\Controllers\CommentController;
 |
 */
 //crée un lien qui permettra aux clients: React, vue, angular, node, js, native
-//récupérer la liste des pièces de théâtre
-Route::get('/theatre', [PostController::class,'index']);
+
+
 //2|xyv1FZzEuEhx7Iltnr3XHKum5hkgcyMisTcAIsjx0103cbbc
 //inscrire un utilisateur
 Route::post('/login',[UserController::class,'login']);
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/logout', [UserController::class, 'logout']);
-Route::delete('/delete/{id}', [UserController::class, 'destroy']);
-Route::put('/update/{id}', [UserController::class, 'update']);
-Route::post('/comments', [CommentController::class, 'store']);
+
 Route::middleware('auth:sanctum')->group(function(){
-    Route::get('theatre', [PostController::class,'index']);
+    Route::delete('/delete/{id}', [UserController::class, 'destroy']);
+    Route::put('/update/{id}', [UserController::class, 'update']);
+    Route::get('/user', [UserController::class, 'user']);
+});
+Route::get('theatre', [PostController::class,'index']);
+Route::middleware('auth:sanctum')->group(function(){
+    //récupérer la liste des pièces de théâtre
+    
     //ajouter une pièce de théâtre /POST /PUT /PATCH
     Route::post('theatre/create',[PostController::class,'store']);
     //modifier une pièce de théâtre /PUT /PATCH
     Route::put('theatre/edit/{theatre}', [PostController::class,'update']);
     //supprimer une pièce de théâtre /DELETE
-    Route::delete('theatre/delete/{theatre}',[PostController::class,'destroy']); 
+    Route::delete('theatre/delete/{theatre}',[PostController::class,'destroy']);
+    //ajouter un commentaire 
+    Route::post('/comments', [CommentController::class, 'store']);
     //retourner l'utilisateur actuellement connecté
     Route::get('/user', function (Request $request) {
         return $request->user();
